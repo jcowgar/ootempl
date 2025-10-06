b45a4492b7c385b8f42028372f56796321b9e0ec
Jeremy Cowgar <jeremy@cowgar.com>
2025-10-06 11:38:25 -0400

    chore(deps): add ex_doc for documentation generation

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Ootempl.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.38", only: :dev, runtime: false},
       {:styler, "~> 1.9", only: [:dev, :test], runtime: false}
     ]
   end
